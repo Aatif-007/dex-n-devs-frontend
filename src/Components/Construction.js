00c7e5b5f6812a4ee297b350f7c535b704fb352e
Aatif-007 <aatifkhan2772@gmail.com>
2024-10-24 22:56:08 +0530

    update

--- src/Components/Construction.js
+++ src/Components/Construction.js
@@ -5,13 +5,13 @@ import infra from "../assets/portfolio/Sindhuratna_infra.JPG";
 import whitestone from '../assets/portfolio/Whitestone.JPG';
 
 const ConstructionImage = [
-        image : {"https://s3-alpha-sig.figma.com/img/ac7e/c439/d5a51e3f28c8eb1832831548b2401b66?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qFifrH~nzmwIN2sfEwI~4Tg459Pt5647mc4BczLs5fI2dZXqOgqMoYRax6WV0WRmnNBUedAyMSe9HR1xsOhW-ca3Y5tRWfPGTjad8JikbQFMAegGDye-9dJ5IajxrzyhlvFmkT8eJICw6ZGQQ36SjIqKR7fKcKiGTnKoZuC92K9SSMgQEIQsDeTi5M9IpWUs~9J~bqTfpJEcHbL4DkEMemCMMA7tHjJrJoUVuHfBJv0hJYgJeOkfZiVp3HTxy9ReVPE61adQfzUGycSlvPfQvYcYkaHpEd8z0BIIylkZaVhEJPqSpBsgAG6SW070n-x2Gz7Zqkr6JoKxFOx~Ui-5TA__"},
-        ,
-        image : {
-                
-        }
-
-    
+        
+                gss,
+                kiwi,
+                sankalp,
+                infra,
+                whitestone
+        
 ]
 
 export default ConstructionImage
