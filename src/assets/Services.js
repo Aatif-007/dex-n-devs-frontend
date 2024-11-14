81c4f3bf605b069c2fc631ba120b9655c1f7e4f4
Aatif-007 <aatifkhan2772@gmail.com>
2024-11-14 22:20:49 +0530

    Some more

--- src/assets/Services.js
+++ src/assets/Services.js
@@ -1,29 +1,33 @@
 import imagyy from '../assets/ExpertiseImages/Saly-44 (1).png'
-
+import webDev from "../assets/OurExpertise/web-dev.png"
+import uxDesigning from "../assets/OurExpertise/ux-designing.png"
+import digital from "../assets/OurExpertise/digital.png"
+import graphic from "../assets/OurExpertise/graphic.png"
+import product from "../assets/OurExpertise/product.png"
 export const ServiceData = [
     {
         id : 1,
         text : "Web Development",
-        image : "https://s3-alpha-sig.figma.com/img/f292/22f2/0dadda15819562ae90af3be3b9daefba?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mQwU7Tb97oLGBNhTdXiR4oe~DS~--P9Dlbc4wYJKzPaxM6z7BzbCJLgURQ2qsaNvw0xdGqTTCcIFfMtI0XU1E27MMYFrZtuIQvIbSvx1vZ8EWRam1Rz69Z94BnbV50RxmZSxp~Z2NTQPaco5zmqU50BW60Af-IawszClZkJA-Cznz0CqN4eO9JoPlw6S1XrB0Uw7hDiHJ7e-t6kvAs1b3aiwkiymYG8w96q3TJEqBYOEqClLKlO6AZRc7AAd2TdfureimvCqasQRNmJEspOSz7IhCyaTTUXVNbkwf3HRVuONvYLM1ang1R7WjC0UTHqL~8rMz26a9eHthPP5tMOZzw__",
+        image : webDev,
         desc : "We specialize in crafting visually stunning, highly functional websites tailored to your unique needs. Our services encompass everything from intuitive design and seamless user experiences to robust back-end development .Whether you're a startup, small business, or e-commerce venture, we deliver tailored solutions that enhance your online presence and drive results. Let us turn your vision into a powerful digital experience."
     },
     {
         id : 2,
         text : "UI/UX Designing",
-        image : "https://s3-alpha-sig.figma.com/img/3fb0/d000/a3543d77efeb86878d4d7677998b4b3a?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Fd72IryOw6d287-j9xsSmkx9QjBJD5huZ7q8eSowm3JlHM76nZJnr2UO25K9gPBgo9lAk5Oct8AGPP33feSrVtJO1c4kSa9R6ttpJsXuDcuuICbOju14OuqPGJhkrg9lU9ctbXFyf7SefA5-GP3Wrtu4XOlokkVJY-lZQtz6OeZhKrdTWgK1cNyjXIjNcobXGbNhC8JoSC-I8Gw4nLId~ygp70ujGPJckzu7CDL4UzySK~m99OI3T5gQ1dTuDTPyIjawSX-jTLrMcbwFFpgiLsZ9UIE7cCUG44LM6yFMCp1ooS8q9r~CHdV31vO6zwervl4LijV5a4wd3cUF5bwX7w__",
+        image : uxDesigning ,
         desc : "Our UI/UX design transforms digital experiences through user-centered design and innovative interfaces. We specialize in user research, wireframing, and visual design to create intuitive and engaging products. Partnering with both startups and established companies, we enhance usability and visual appeal. Our approach ensures that every design is both functional and aesthetically pleasing, driving user satisfaction and business success."
     },
     {
         id : 3,
         text : "Product Designing",
-        image : 'https://s3-alpha-sig.figma.com/img/19fe/b41e/0111b50387a3736fdd3bcb10684919b8?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W~LfxyeJ6TDN3NUuZUhxXjq0JYS22EPR0Y518GtifUfH432itjtN6nJd65~pqSRzvo6CduSXV6aYkCeXixONuh763EAyIfnaOk0SIVacTTDlkSF1ONKHw9eaE0PAiafCcSwH8vZnLPxz9z2hVd6MmcAs03HnbEaBPCENGaDb7guoOgx8LJaLyiu1M49ohZfQjdktwTYOjkdPflqADCRaI9hdKNdprnfJ8RjJ8xrYJVWMhGmlD9taVAjTa9yMhxbRjz3PvwShmIIdS59w-mtfpz5LfhgAG1gUGQsjzdiKGZTDjsB~AO1~dhq1v0wY1SKL~0jLmnYdEM-J~LXFrptnMA__',
+        image : product,
         desc : "Our product design business excels in crafting innovative, user-centered solutions that blend form and function. We focus on every stage of the design process, from concept development and prototyping to final execution. By leveraging in-depth research and creative problem-solving, we deliver products that captivate users and meet market needs. Our commitment is to enhance usability and aesthetics, driving success for our clients."
 
     },
     {
         id : 4,
         text : "Digital Marketing",
-        image : "https://s3-alpha-sig.figma.com/img/edca/2161/494d40bfd800578d8c7aa974d3d94d44?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EyJ3nFLDpYIXjxDHXUm9pQst0coIAK9jShZbUNKoCuk47j7pmdTk-5SDyaqxJnfynyu1SYrPgJ8MOvuDc23V05mfvlzB3yZBo-kPn1Wq32pNEvFWHqubQ0T0o4fFN46dZ0CGJEPpra~Ni3hX0aCVQ-YDRJC5Ey7nV8E8SqE~ECIkHZ-e2ucddp9AukdQoeotMHOqaJRB9dvSrAb1Y9OH-mxh9et7CHIYW7UtuO-4xvBJbsVx7M-ihsXGGbWfFNixFDZEaJg-qa2PCi8-jcwkpCusV6G-GHBUVxd7Mf2wx8gu8Vm1ku3~imItbAYqBOJT3xvchBACcFMnkMclV3T5og__",
+        image : digital,
         desc : "Transform your online presence with dexNdevs. We specialize in creating custom digital marketing solutions that integrate SEO, compelling content, and effective social media management to connect your brand with the right audience and achieve your business goals."
     },
     {
@@ -35,7 +39,7 @@ export const ServiceData = [
     {
         id : 6,
         text : "Graphic Designing",
-        image : "https://s3-alpha-sig.figma.com/img/86bf/2359/b7670b661e7ea345eda8be5c0ebae7ae?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NP45K5tkrU8Gem8fnuPavJ7cGlyifvLY9bpU05~kLMks1cdjtceblVyqqB-YqSlocKbCC9lnUiJ8SLkbqbDlilRNjgw5MorF4EYgmoXStzwiK51OYf1FnHJZXbqdpAeaSY6WwsIjWrYdGS3-zUw2lQya26vrCYS1VpSSTvPAHA8tlmVH4r6Dhh2XdMpMWjhPSlhSGw4fioDpyGe-FG85RTShjNWXr2OaW3lC~bE1l3F4egURbRz~OVQOlglr~KDRF~Dk0NZHP3mXPaZ1PkpObWzZgCULNTmAXE15A4cIaCXSAdBl1hb4JhNzK44PDtLKPyJRXtv5VMbC6Q1qR8bQHA__",
+        image : graphic,
         desc : "We bring your brand's vision to life with striking and memorable graphic designs. Our expertise spans from dynamic logos to compelling marketing collateral, blending creativity with strategic insight to make your message stand out. We are dedicated to delivering designs that capture attention and resonate with your audience."
     }
 ]
